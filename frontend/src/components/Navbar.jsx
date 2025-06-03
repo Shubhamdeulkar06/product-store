d4d5f45dfdfbc293077e2ec2964955f44d883427
Shubhamdeulkar06 <shuboy0868@gmail.com>
2025-06-03 12:08:05 +0530

    created basic frontend structure with nav bar using chakra ui

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Button, Center, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+import {PlusSquareIcon} from '@chakra-ui/icons';
+import { LuSun } from "react-icons/lu";
+import { IoMoon } from "react-icons/io5";
+
+
+const Navbar = () => {
+    const {colorMode,toggleColorMode} = useColorMode();
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Text
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+          fontSize={{base:"22", sm:"28"}}
+          fontWeight="bold"
+          textAlign={"center"}
+          textTransform={"uppercase"}
+        >
+          <Link to={"/"} >Product Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+            <Link to={"/create"} >
+                <Button>
+                    <PlusSquareIcon fontSize={20}/>
+                </Button>
+            </Link>
+            <Button onClick={toggleColorMode}>
+                {colorMode === 'dark' ? <LuSun size="20"/> : <IoMoon size="20"/>}
+            </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
